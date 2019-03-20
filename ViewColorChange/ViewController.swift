bfc0cc5763a63efe26109b8d5de51b6ec3c411c2
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-03-20 15:29:27 +0900

    isHidden

--- ViewColorChange/ViewController.swift
+++ ViewColorChange/ViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redv: UIView!
+    @IBOutlet weak var bluev: UIView!
+    @IBOutlet weak var greenv: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.lightGray
+        //self.view.backgroundColor = UIColor.lightGray
     }
-
-
+    @IBAction func b1(_ sender: Any) {
+        //redv.backgroundColor = UIColor.white
+        redv.isHidden = true
+    }
+    
+    @IBAction func b2(_ sender: Any) {
+        //bluev.backgroundColor = UIColor.white
+        bluev.isHidden = true
+    }
+    
+    @IBAction func b3(_ sender: Any) {
+        //greenv.backgroundColor = UIColor.white
+        greenv.isHidden = true
+    }
+    
 }
 
